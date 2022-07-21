61a2d9ab88fee56d9a2510cf612734ee440e675d
Guillaume Egles <gegles@us.ibm.com>
2022-07-20 21:59:13 -0700

    refactor: improve conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,35 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from conans import ConanFile, CMake
+
+from conan import ConanFile
+from conan.tools.cmake import CMake
+from conan.tools.build import check_min_cppstd
 
 class SpdlogSetupConan(ConanFile):
-    name = 'spdlog_setup'
-    version = '1.1.0'
-    settings = 'os', 'arch', 'compiler', 'build_type'
-    exports_sources = 'include/*', 'test/*', 'CMakeLists.txt'
+    name = "spdlog_setup"
+    version = "1.1.0"
+    description = "TOML config for spdlog"
+    url = "https://github.com/gegles/spdlog_setup"
+    topics = ("spdlog", "logging", "header-only", "TOML", "cpptoml")
+    license = "MIT"
+
+    settings = "os", "arch", "compiler", "build_type"
+    exports_sources = "include/*", "test/*", "CMakeLists.txt"
     no_copy_source = True
-    generators = 'CMakeToolchain', 'CMakeDeps'
+    generators = "CMakeToolchain", "CMakeDeps"
 
     def config_options(self):
-        self.options['fmt'].header_only = True
-        self.options['spdlog'].header_only = True
+        self.options["fmt"].header_only = True
+        self.options["spdlog"].header_only = True
 
     def build_requirements(self):
-        self.tool_requires('catch2/[>=3.0.1]')
+        self.test_requires("catch2/[>=3.0.1]")
 
     def requirements(self):
-        self.requires('cpptoml/0.1.1')
-        self.requires('spdlog/1.10.0')
-        self.requires('fmt/[>=8.1.1]')
+        self.requires("cpptoml/0.1.1")
+        self.requires("spdlog/1.10.0")
+        self.requires("fmt/[>=8.1.1]")
 
-    def build(self): # this is not building a library, just tests
+    def validate(self):
+        check_min_cppstd(self, 17)
+
+    def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
         cmake.test()
 
     def package(self):
-        self.copy('*.hpp')
+        self.copy("*.hpp")
 
     def package_id(self):
-        self.info.header_only()
+        self.info.clear()
