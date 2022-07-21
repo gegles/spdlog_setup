5ef81eca0646f65437827a25ed7882af292a3213
Guillaume Egles <gegles@us.ibm.com>
2022-07-20 18:43:41 -0700

    build: bump catch version

--- conanfile.py
+++ conanfile.py
@@ -4,22 +4,24 @@ from conans import CMake
 from conan import ConanFile
 
 class SpdlogSetupConan(ConanFile):
-    name = "spdlog_setup"
-    version = "1.1.0"
-    settings = "os", "arch", "compiler", "build_type"
-    exports_sources = "include/*", "test/*", "CMakeLists.txt"
+    name = 'spdlog_setup'
+    version = '1.1.0'
+    settings = 'os', 'arch', 'compiler', 'build_type'
+    exports_sources = 'include/*', 'test/*', 'CMakeLists.txt'
     no_copy_source = True
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = 'CMakeToolchain', 'CMakeDeps'
 
     def config_options(self):
         self.options['fmt'].header_only = True
         self.options['spdlog'].header_only = True
 
     def requirements(self):
-        self.requires("catch2/3.0.1")
-        self.requires("cpptoml/0.1.1")
-        self.requires("spdlog/1.10.0")
-        self.requires("fmt/8.1.1")
+        self.requires('cpptoml/0.1.1')
+        self.requires('spdlog/1.10.0')
+        self.requires('fmt/8.1.1')
+
+    def build_requirements(self):
+        self.tool_requires('catch2/3.1.0')
 
     def build(self): # this is not building a library, just tests
         cmake = CMake(self)
@@ -28,7 +30,7 @@ class SpdlogSetupConan(ConanFile):
         cmake.test()
 
     def package(self):
-        self.copy("*.hpp")
+        self.copy('*.hpp')
 
     def package_id(self):
         self.info.header_only()
